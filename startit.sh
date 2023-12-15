285ab070231a2ece55c9162c145b9a6f7b0c5f42
Thodoris Velmachos <thodoris.velmachos@suite5.eu>
2023-12-15 22:24:31 +0200

    initial commit

--- startit.sh
+++ startit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export PYTHONPATH="<specify>"
-export SELECTED_INDICES="<specify>"
+export PYTHONPATH="<specify>" # set the current absolutepath
+export SELECTED_INDICES="<specify>" # you can select one of the following ways to specify index  ("all","index_name","index_name1,index_name2")
 # export SELECTED_INDICES="all"
 export SELECTED_ENVIRONMENT="<specify>"
 export ES_AUTH_USERNAME="<specify>"
